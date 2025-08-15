a62f34daa45288e6744bc8cbc561f348b6f29fd4
Osneil Drakes <osneil-drakes@hotmail.com>
2025-08-15 11:32:49 +0100

    feat: set up workflow to deploy to pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/waltham-forest-cardinals-site/", // Replace with your actual repo name
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
